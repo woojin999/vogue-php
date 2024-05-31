4c0fd0991b876acf4ab099593628df37acd3f264
LEE WOOJIN <24864@naver.com>
2024-05-31 12:49:51 +0900

    Initial commit

---
+++ vogue/js/components/Gallery.jsx
@@ -0,0 +1,115 @@
+// 보그 JS 갤러리 컴포넌트 -Gallery.jsx
+
+export default function Gallery() {
+  // 스와이퍼 인스턴스 변수
+  let swiper;
+  // 스와이퍼 인스턴스 생성함수 //
+  const setSwiper = () => {
+    swiper = new Swiper(".mySwiper", {
+      // 한 화명당 슬라이드 수(아래 breakpoint로 설정함)
+      slidesPerView: 1,
+      // 슬라이드 사이간격
+      spaceBetween: 10,
+      // 무한넘기기
+      loop: true,
+      // 자동넘기기
+      autoplay: {
+        // 사이간격
+        delay: 2500,
+        // 상호작용 죽이기 속성 없앰
+        disableOnInteraction: false,
+      },
+      // 하단 블릿표시
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // 양쪽이동버튼
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        700: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1000: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      },
+    });
+  }; /// setSwiper
+  // 화면 랜더링전 실행구역//
+  React.useLayoutEffect(() => {
+    // 스와이퍼 생성 함수 호출
+    setSwiper();
+    // 스와이퍼 API 테스트
+    // 갤러리 제목(.stit) 클릭시 다음슬라이드 넘기기
+    $(".stit").click(() => {
+      // 변수에 담은 스와이퍼 인스턴스의 구체적인 API 메서드를 호출하여 기능을 수행한다
+      swiper.slideNext();
+      // swiper.slidePrev();
+      // 스와이퍼 API는 여기서 참조
+      // https://swiperjs.com/swiper-api
+    });
+  }); // useLayoutEffect ////////
+
+  // 코드 리턴구역 //
+  return (
+    <React.Fragment>
+      <div id="main-area">
+        <main className="main-area ibx">
+          {/* 2-1. 로그인 페이지 상단영역 */}
+          <header className="ctop">
+            {/* 2-1-1. 서브타이틀 */}
+            <h2 className="stit">Gallery</h2>
+          </header>
+          {/* 2-2. 갤러리 페이지 컨텐츠 박스 */}
+          <section className="scont">
+            {/* <!-- Swiper --> */}
+            <div className="swiper mySwiper">
+              <div className="swiper-wrapper">
+                <div className="swiper-slide">
+                  <img src="images/people/cont2-1a.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont1-2b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/people/cont2-3a.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont2-1b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont2-2b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/fashion/cont2-3b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/beauty/cont1-2a.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/beauty/cont2-3b.jpg" alt="보그갤러리" />
+                </div>
+                <div className="swiper-slide">
+                  <img src="images/beauty/cont1-1b.jpg" alt="보그갤러리" />
+                </div>
+              </div>
+              <div class="swiper-button-next"></div>
+              <div class="swiper-button-prev"></div>
+              <div className="swiper-pagination"></div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </React.Fragment>
+  );
+} ///////////// Gallery 컴포넌트 ////////////
---
+++ vogue/js/main.js
@@ -0,0 +1,70 @@
+// 보그 PJ 메인 JS - main.js
+
+// 상단영역 불러오기
+import TopArea from "./components/TopArea";
+// 메인영역 불러오기
+import MainArea from "./components/MainArea";
+// 아이템영역 불러오기
+import ItemsArea from "./components/ItemsArea";
+// 하단영역 불러오기
+import FooterArea from "./components/FooterArea";
+// 갤러리 모듈 불러오기
+import Gallery from "./components/Gallery";
+// 로그인 모듈 불러오기
+import Login from "./components/Login";
+// 회원가입 모듈 불러오기
+import Member from "./components/Member";
+
+// [1] 메인 페이지 전체 레이아웃 로딩 컴포넌트 /////
+function Layout() {
+  // 상태관리변수 설정구역 /////
+  // [1] 메뉴 변경 상태변수
+  const [menu, setMenu] = React.useState("home");
+
+  // 화면 랜더링 직전에 CSS로딩 변경하기 ////
+  React.useLayoutEffect(() => {
+    // menu 상태변수에 의존시킨다
+    // 메인 css 대상요소 : #main-css
+    document.querySelector("#main-css").href =
+      menu == "home"
+        ? "./css/main.css"
+        : menu == "gallery"
+        ? "./css/gallery.css"
+        : menu == "login"
+        ? "./css/login.css"
+        : menu == "member"
+        ? "./css/member.css"
+        : "./css/items.css";
+        
+        // 페이지 최상단이동코드
+        window.scrollTo(0,0)
+  }, [menu]);
+  // menu값이 :home인 경우 main.css
+  // menu값이 gallery인 경우 gallery.css
+  // menu값이 login인 경우 login.css
+  // 기타 메뉴인경우 items.css
+  // 코드 리턴구역 /////
+  return (
+    <React.Fragment>
+      {/* 1. 상단영역 컴포넌트 */}
+      <TopArea changeMenu={setMenu} />
+      {/* 2. 메인영역 컴포넌트 */}
+      {menu == "home" ? (
+        <MainArea />
+      ) : menu == "gallery" ? (
+        <Gallery />
+      ) : menu == "login" ? (
+        <Login changeMenu={setMenu} />
+      ) : menu == "member" ? (
+        <Member changeMenu={setMenu} />
+      ) : (
+        <ItemsArea catName={menu} />
+      )}
+      {/* 3. 하단영역 컴포넌트 */}
+      <FooterArea />
+    </React.Fragment>
+  );
+} ////Layout 컴포넌트 ///////////////
+
+// 메인페이지 전체출력하기
+ReactDOM.render(<Layout />, document.querySelector("#root"));
